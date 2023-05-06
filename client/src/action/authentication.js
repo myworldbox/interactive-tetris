34dd958bfe37f16d7915a2f38590b4f2ac654e4d
myworldbox <54nicholasleung45@gmail.com>
2023-05-06 23:04:02 +0800

    %npm_config_comment%

--- client/src/action/authentication.js
+++ client/src/action/authentication.js
@@ -1,11 +1,13 @@
-const Authentication = (function () {
+const Authentication = (() => {
   let user = null;
 
-  const getUser = () => {
-    return user;
+  const setUserInfo = (json) => {
+    user = json.user;
+    document.getElementById("name-left").innerHTML = user.name;
+    document.getElementById("username-left").innerHTML = user.username;
   };
 
-  const signin = function (username, password, onSuccess, onError) {
+  const signin = (username, password, onSuccess, onError) => {
     const json = JSON.stringify({ username, password });
 
     fetch("/signin", {
@@ -15,13 +17,10 @@ const Authentication = (function () {
     })
       .then((res) => res.json())
       .then((json) => {
-        if (json.status == "error") {
+        if (json.status === "error") {
           if (onError) onError(json.error);
         } else {
-          user = json.user;
-          document.getElementById("name-left").innerHTML = user.name;
-          document.getElementById("username-left").innerHTML = user.username;
-
+          setUserInfo(json);
           if (onSuccess) onSuccess();
         }
       })
@@ -30,23 +29,21 @@ const Authentication = (function () {
       });
   };
 
-  const validate = function (onSuccess, onError) {
+  const validate = (onSuccess, onError) => {
     fetch("/validate")
       .then((res) => res.json())
       .then((json) => {
-        if (json.status === "success") {
-          if (json.user) {
-            user = json.user;
-            document.getElementById("name-left").innerHTML = user.name;
-            document.getElementById("username-left").innerHTML = user.username;
-            onSuccess();
-          }
-        } else if (onError) onError(json.error);
+        if (json.status === "success" && json.user) {
+          setUserInfo(json);
+          if (onSuccess) onSuccess();
+        } else if (json.status === "error" && onError) {
+          onError(json.error);
+        }
       })
       .catch((err) => {
         console.log("Error on Validation!");
       });
   };
 
-  return { getUser, signin, validate };
+  return { getUser: () => user, signin, validate };
 })();
